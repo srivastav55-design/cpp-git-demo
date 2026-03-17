abb582b25f5c8df692d16f41b6879f45d9647524
Srivatsav Gowda <srivatsavgowda@Mac.lan>
2026-03-17 23:09:21 +0530

    Updated message in feature branch

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
+
+int sum(int *a, int *b) {
+    return *a + *b;
+}
+
 int main() {
 
-    int a = 15, b = 15;
-    int sum = a + b;
-    cout << "updated sum program " << sum << endl;
+    int a = 5, b = 12;
+    int *ptr_a = &a;
+    int *ptr_b = &b;
+    int c =sum(&a, &b);
+    cout << "sum is" << c << endl;
     return 0;
 }
