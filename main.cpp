5e7cd8cecbd63d67f143ceaed8702027cfd88077
Srivatsav Gowda <srivatsavgowda@Mac.lan>
2026-03-17 22:45:24 +0530

    Added C++ program to add two numbers

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 15, b = 15;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
