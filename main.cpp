693031eb42aebd66325f5d3b7ebbecc5fa80d83e
srivastav55-design <srivastav55@gmail.com>
2026-03-17 22:58:44 +0530

    Update output message in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main() {
 
     int a = 15, b = 15;
     int sum = a + b;
-    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+    cout << "updated sum program " << sum << endl;
     return 0;
 }
